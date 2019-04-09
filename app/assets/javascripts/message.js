c7a4339fd0b1148219b7631fc641f3af50cafe57
Junki Kikuchi <jkikuchi00@gmail.com>
2019-04-09 23:56:54 +0900

    Add asynchronous message sending function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function(){
+  function scroll(){
+    var target = $('.message').last();
+    var position = target.offset().top + $('.messages').scrollTop();
+    $('.messages').animate({scrollTop:position}, 500, 'swing');
+  }
+  function buildHTML(content){
+    var message = content.message ? `${content.message}` : "";
+    var image = content.image ? `<img src=${content.image}>` : "";
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                    ${content.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                    ${content.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                  ${message}
+                  </p>
+                  ${image}
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+       var html = buildHTML(data);
+       $('.messages').append(html);
+       scroll();
+       $('#message_message').val("");
+       $('#message_image').val("");
+    })
+    .fail(function(){
+      alert('メッセージを送信できませんでした！！');
+    })
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.user_name @message.user.name
+json.date @message.created_at.in_time_zone('Asia/Tokyo').strftime("%Y/%m/%d(%a) %H:%M")
+json.message @message.message
+json.image @message.image.url
